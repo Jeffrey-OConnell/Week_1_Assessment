0266810c96efc4c4a14bfd4a2feb585131fa0bed
Jeff OConnell <jeffreytoconnell@icloud.com>
2022-07-15 08:42:51 -0700

    Added weak passwords

--- password.js
+++ password.js
@@ -1,6 +1,8 @@
-let password = "Coding123456"
+let password = "Coding12345"
 
-if(password.length < 10){
+if(password === "password" || password === "letmein"){
+    console.log("Weak password. Try again.")
+} else if(password.length < 10){
     console.log("Password not long enough")
 } else if(/[A-Z]/.test(password) && /[a-z]/.test(password) && /[0-9]/.test(password))   {  
     console.log("Password Validated")
