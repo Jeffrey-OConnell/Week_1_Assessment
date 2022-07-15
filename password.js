2a5038ed8e8ff2eb228eded093535da04cde772a
Jeff OConnell <jeffreytoconnell@icloud.com>
2022-07-15 08:22:18 -0700

    password.js complete

--- password.js
+++ password.js
@@ -1,5 +1,9 @@
-let password = "coding123456"
+let password = "Coding123456"
 
 if(password.length < 10){
     console.log("Password not long enough")
-} else if()
+} else if(/[A-Z]/.test(password) && /[a-z]/.test(password) && /[0-9]/.test(password))   {  
+    console.log("Password Validated")
+} else {
+    console.log("Password must contain at least 1 uppercase, 1 lowercase, and a number")
+}
